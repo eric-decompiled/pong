8e9110041542dd86c31f67aade7ece39468233f9
Eric Richardson <eric@decompiled.dev>
2024-02-17 19:50:32 -0500

    normalize canvas ratio to device pixels

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import './style.css';
 import { Paddle } from './js/paddle';
 
 const canvas = document.querySelector('#canvas');
+
+// normalizes canvas ratio to avoid pixelation
+const devicePixelRatio = window.devicePixelRatio || 1;
+canvas.width = 960 * devicePixelRatio;
+canvas.height = 540 * devicePixelRatio;
+
 const c = canvas.getContext('2d');
 
 if (!c) {
